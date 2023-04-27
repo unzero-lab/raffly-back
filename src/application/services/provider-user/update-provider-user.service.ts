485d4b47c3a00fcd3c708984a2ca4e76666c5ca3
Mykão <mykewillow@gmail.com>
2023-04-27 20:52:23 -0300

    refactor: deleted comment and changed error

--- src/application/services/provider-user/update-provider-user.service.ts
+++ src/application/services/provider-user/update-provider-user.service.ts
@@ -1,21 +1,3 @@
-/* 
-1- importar updateUserRepository
-2- importar os UseCase's
-3- importar o Inject e o Injectable
-4- fazer o @injectable() e imediatamente em baixo 
-exportar uma classe com nom UpdateProviderUserService que vai implementar o UpdateProviderUserUseCase
-abrir uma chave e criar um constructor
-com o @Inject abrir parênteses e escrever o updateProviderUserRepository
-colocar uma 'chave' privada chamada private e uma 'placa' sinalizando readonly para uma propriedade que nao pode ser alterada que é a providerUserRespository que vai armazenar o UpdateProviderUserRepository depois so abrir uma chave e passar para a proxima etapa
-fazemos um função assícrona publica com o metodo execute que veio la da interface feita no caso de uso com o (params: UpdateProviderUserUseCasePamrs) que vai receber uma Promisie do tipo <UpdateProviderUserUseCaseResult> abrimos a chave {} e dentro vamos fazer uma const changeProvideruser 
-e nessa constante vamos por um await e chamar providerUserRepository de dentro da classe UpdateProviderUserService com o this e mais um . pra chamar o updateProviderUser que fizemos la no repository e por params dentro de parenteses
-depois fazemos um if (!updateProviderUser) {
-    return new Error('Error  when updating provider user')
-}
-
-return updateProviderUser
-*/
-
 import { UpdateProviderUserRepository } from '@/application/contracts/repositories/provider-user'
 import {
   UpdateProviderUserUseCase,
@@ -33,8 +15,8 @@ export class UpdateProviderUserService implements UpdateProviderUserUseCase {
   public async execute(params: UpdateProviderUserUseCaseParams): Promise<UpdateProviderUserUseCaseResult> {
     const updatedProviderUser = await this.providerUserRepository.updateProviderUser(params)
 
-    if (!updatedProviderUser) {
-      return new Error('Error when updating provider User')
+    if (updatedProviderUser instanceof Error) {
+      return updatedProviderUser
     }
     return updatedProviderUser
   }
